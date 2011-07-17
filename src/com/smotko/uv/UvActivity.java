0c6ceb4adca8cff2c5acd372bc75ae157f99f6ab
Smotko <ensmotko@gmail.com>
2011-07-17 21:28:50 +0200

    Managed to open an URL

--- src/com/smotko/uv/UvActivity.java
+++ src/com/smotko/uv/UvActivity.java
@@ -1,10 +1,20 @@
 package com.smotko.uv;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.LabeledIntent;
 import android.location.*;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class UvActivity extends Activity {
@@ -15,10 +25,29 @@ public class UvActivity extends Activity {
         setContentView(R.layout.main);
         TextView label = (TextView) findViewById(R.id.label);
     	label.setText("GETTING LOCATION");
+    	
         getLocation();
+
+
+
                 
     }
     
+    public void getData() throws Throwable{
+	   URL url = new URL("http://smotko.si");
+	   URLConnection urlConnection = url.openConnection();
+	   BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	   StringBuilder builder = new StringBuilder();
+	   String line = "";
+
+	   TextView label = (TextView) findViewById(R.id.label);
+	   while ((line = br.readLine()) != null) {
+		   
+	       builder.append(line);
+	   }
+	   String result = builder.toString();
+	   label.setText(result);
+    }    
     public void getLocation(){
 		// Acquire a reference to the system Location Manager
 	    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -53,9 +82,20 @@ public class UvActivity extends Activity {
     
     public void gotLocation(Location location){
     	TextView label = (TextView) findViewById(R.id.label);
+    	
+        try {
+			getData();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	/*
     	label.setText("GOT LOCATION: Accuracy: " + Float.toString(location.getAccuracy())
     			    + " Latitude: " + Double.toString(location.getLatitude())
     			    + " Longitude: " + Double.toString(location.getLongitude())
     			    + " Altitude: " + Double.toString(location.getAltitude()));
+    	*/
     }
 }

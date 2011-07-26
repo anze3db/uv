2b4cccb6d3f205fda8b40806810170e064b58f47
Smotko <ensmotko@gmail.com>
2011-07-26 16:04:23 +0200

    Fixed imports

--- src/com/smotko/uv/UvActivity.java
+++ src/com/smotko/uv/UvActivity.java
@@ -1,20 +1,15 @@
 package com.smotko.uv;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-
 import android.app.Activity;
 import android.content.Context;
-import android.location.*;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class UvActivity extends Activity {
